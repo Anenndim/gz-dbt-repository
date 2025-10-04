bdb44c26417ab833df503ea2cb59ac5b310c504b
Abderrahman Guenna <abdou.guenna@gmail.com>
2025-10-04 21:40:45 +0000

    commit and sync

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,10 @@
-SELECT date_date,
-    products_id,
-    (quantity * purchase_price) AS purchase_cost,
-    ROUND((revenue - (quantity * purchase_price)), 2) AS margin
-FROM  {{ ref('stg_raw__sales') }}
-INNER JOIN {{ ref('stg_raw__product') }}
-USING(products_id)
+SELECT
+    s.date_date,
+    p.products_id,
+    ROUND(s.quantity * p.purchase_price, 2) AS purchase_cost,
+    ROUND(s.revenue - (s.quantity * p.purchase_price), 2) AS margin,
+    {{ margin_percent('s.revenue', 's.quantity * p.purchase_price') }} AS margin_ratio
+FROM {{ ref('stg_raw__sales')}} AS s
+INNER JOIN {{ ref('stg_raw__product')}} AS p
+    ON s.products_id = p.products_id
+
